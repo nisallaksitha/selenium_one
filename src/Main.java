efd5f28d49e5bd8c55d76970f595f118620b54cb
Your Name <mnisalcosta@gmail.com>
2023-10-10 13:24:36 +0530

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new FirefoxDriver();
+        driver.get("https://www.saucedemo.com");
+        driver.findElement(By.id("user-name")).sendKeys("standard_user");
+        driver.findElement(By.id("password")).sendKeys("secret_sauce");
+        driver.findElement(By.id("login-button")).click();
+        driver.wait(100000);
+    
+        }
+    }
